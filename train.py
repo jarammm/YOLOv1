1dca3ceff0010df848f3481c70a2bff442281863
jarammm <jaramrammm@gmail.com>
2023-02-19 00:29:46 +0900

    change backbone darknet -> resnet18

--- train.py
+++ train.py
@@ -1,152 +1,20 @@
-"""
-Main file for training Yolo model on Pascal VOC dataset
-"""
-
-import time
-import torch
-import torchvision.transforms as transforms
-import torch.optim as optim
-import torchvision.transforms.functional as FT
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-from model import Yolov1
-from dataset import VOCDataset
-from utils import (
-    non_max_suppression,
-    mean_average_precision,
-    intersection_over_union,
-    cellboxes_to_boxes,
-    get_bboxes,
-    plot_image,
-    save_checkpoint,
-    load_checkpoint,
-)
-from loss import YoloLoss
-
-seed = 123
-torch.manual_seed(seed)
-
-# Hyperparameters etc. 
-LEARNING_RATE = 2e-5
-DEVICE = "cuda" if torch.cuda.is_available else "cpu"
-BATCH_SIZE = 32 # 64 in original paper but I don't have that much vram, grad accum?
-WEIGHT_DECAY = 0.0005
-EPOCHS = 135
-NUM_WORKERS = 4
-PIN_MEMORY = True
-LOAD_MODEL_FILE = "model_data/yolo_weights.h5"
-IMG_DIR = "data/images"
-LABEL_DIR = "data/labels"
+from utils import load_data_voc
+from yolo import *
+from torch import nn
+import torchvision
 
 
-class Compose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
+train_iter, test_iter, test_iter_raw = load_data_voc(batch_size=16, download=True)
 
-    def __call__(self, img, bboxes):
-        for t in self.transforms:
-            img, bboxes = t(img), bboxes
+resnet18 = torchvision.models.resnet18(pretrained=True)
+backbone = nn.Sequential(*list(resnet18.children())[:-2]) # remove avg pool and fc
+net = Yolo(backbone, backbone_out_channels=512)
 
-        return img, bboxes
+def lr(epoch):
+    if epoch < 10: return 0.001 * (epoch + 1)
+    if epoch < 85: return 0.01
+    if epoch < 115: return 0.001
+    if epoch < 135: return 0.0001
 
+train(net, train_iter, test_iter, 135, lr=lr, momentum=0.9, weight_decay=5e-4, accum_batch_num=4, save_path='./model', pretrained=True)
 
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
-
-
-def train_fn(train_loader, model, optimizer, loss_fn):
-    loop = tqdm(train_loader, leave=True)
-    mean_loss = []
-
-    for batch_idx, (x, y) in enumerate(loop):
-        x, y = x.to(DEVICE), y.to(DEVICE)
-        out = model(x)
-        loss = loss_fn(out, y)
-        mean_loss.append(loss.item())
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # update progress bar
-        loop.set_postfix(loss=loss.item())
-
-    print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
-
-
-def main():
-    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
-    optimizer = optim.Adam(
-        model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-    )
-    loss_fn = YoloLoss()
-
-    train_dataset = VOCDataset(
-        "data/train.csv",
-        transform=transform,
-        img_dir=IMG_DIR,
-        label_dir=LABEL_DIR,
-    )
-
-    test_dataset = VOCDataset(
-        "data/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
-    )
-
-    train_loader = DataLoader(
-        dataset=train_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        shuffle=True,
-        drop_last=True,
-    )
-
-    test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        shuffle=True,
-        drop_last=True,
-    )
-
-    best_mean_avg_prec = 0
-
-    for epoch in range(EPOCHS):
-
-        pred_boxes, target_boxes = get_bboxes(
-            train_loader, model, iou_threshold=0.5, threshold=0.4
-        )
-
-        mean_avg_prec = mean_average_precision(
-            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
-        )
-        print(f"Train mAP: {mean_avg_prec}")
-
-        if mean_avg_prec > best_mean_avg_prec:
-           best_mean_avg_prec = mean_avg_prec
-           checkpoint = {
-               "state_dict": model.state_dict(),
-               "optimizer": optimizer.state_dict(),
-           }
-           save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
-           time.sleep(10)
-
-        train_fn(train_loader, model, optimizer, loss_fn)
-        
-    # test start
-
-    # load pretrained model
-    load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
-    
-    # show test result image & scoring
-    pred_boxes, target_boxes = get_bboxes(
-            test_loader, model, iou_threshold=0.5, threshold=0.4, istest=True
-        )
-
-    # test prediction scoring
-    mean_avg_prec = mean_average_precision(
-            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
-        )
-    print(f"Test mAP: {mean_avg_prec}")
-
-if __name__ == "__main__":
-    main()
